9ac3f9e8febcaa51773362a97324a653ecb3fff5
Jameson Welling <jamowelling@gmail.com>
2018-01-16 13:58:45 -0700

    add basic containers and file tree for project

---
+++ src/decliningBudget/DecliningContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DecliningContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Declining Budget Container</h1>
+      </div>
+    );
+  }
+}
+
+export default DecliningContainer;
---
+++ src/invoices/InvoicesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InvoicesContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Invoices Container</h1>
+      </div>
+    );
+  }
+}
+
+export default InvoicesContainer;
