e8e8ce4cc69e982fce25afedb2a7c3c08535979a
Jameson Welling <jamowelling@gmail.com>
2018-01-16 14:50:42 -0700

    hook react up to redux

---
+++ src/invoices/actions.js
@@ -0,0 +1,361 @@
+import * as types from '../constants';
+
+const DB_URL = 'https://postinvoice-api.herokuapp.com';
+
+/*
+* synchronous action creators
+*/
+
+export function addNewLine() {
+  return {
+    type: types.ADD_NEW_LINE,
+  };
+}
+
+export function addItemToInvoice(item, index) {
+  return {
+    type: types.ADD_ITEM_TO_INVOICE,
+    item,
+    index,
+  };
+}
+
+export function addVendorToInvoice(vendor) {
+  return {
+    type: types.ADD_VENDOR_TO_INVOICE,
+    vendor,
+  };
+}
+
+export function clearNewInvoiceData() {
+  return {
+    type: types.CLEAR_NEW_INVOICE_DATA,
+  };
+}
+
+export function deleteInvoiceComplete(invoiceId) {
+  return {
+    type: types.DELETE_INVOICE_COMPLETE,
+    invoiceId,
+  };
+}
+
+export function fetchingComplete() {
+  return {
+    type: types.FETCHING_COMPLETE,
+  };
+}
+
+export function handleError(error) {
+  return {
+    type: types.HANDLE_ERROR,
+    error,
+  };
+}
+
+export function isFetching() {
+  return {
+    type: types.IS_FETCHING,
+  };
+}
+
+export function onChangeDate(date, [period, week]) {
+  return {
+    type: types.ON_CHANGE_DATE,
+    date,
+    period,
+    week,
+  };
+}
+
+export function onChangeInvoiceNumber(number) {
+  return {
+    type: types.ON_CHANGE_INVOICE_NUMBER,
+    number,
+  };
+}
+
+export function onChangeItemAmount(item, index, amount) {
+  return {
+    type: types.ON_CHANGE_ITEM_AMOUNT,
+    item,
+    index,
+    amount,
+  };
+}
+
+export function onChangeItemCode(item, index, codeName) {
+  return {
+    type: types.ON_CHANGE_ITEM_CODE,
+    item,
+    index,
+    codeName,
+  };
+}
+
+export function onChangeVendorName(vendorName) {
+  return {
+    type: types.ON_CHANGE_VENDOR_NAME,
+    vendorName,
+  };
+}
+
+export function postNewInvoiceBegin() {
+  return {
+    type: types.POST_NEW_INVOICE_BEGIN,
+  };
+}
+
+export function postNewInvoiceComplete(invoice) {
+  return {
+    type: types.POST_NEW_INVOICE_COMPLETE,
+    invoice,
+  };
+}
+
+export function removeItemFromInvoice(item) {
+  return {
+    type: types.REMOVE_ITEM_FROM_INVOICE,
+    item,
+  };
+}
+
+export function retrieveInvoicesBegin() {
+  return {
+    type: types.RETRIEVE_INVOICES_BEGIN,
+  };
+}
+
+export function retrieveInvoicesComplete() {
+  return {
+    type: types.RETRIEVE_INVOICES_COMPLETE,
+  };
+}
+
+export function temporaryAddVendor(newVendor) {
+  return {
+    type: types.TEMPORARY_ADD_VENDOR,
+    newVendor,
+  };
+}
+
+export function updateCodes(codes) {
+  return {
+    type: types.UPDATE_CODES,
+    codes,
+  };
+}
+
+export function updateInvoices(invoices) {
+  return {
+    type: types.UPDATE_INVOICES,
+    invoices,
+  };
+}
+
+export function updateVendorList(vendors) {
+  return {
+    type: types.UPDATE_VENDOR_LIST,
+    vendors,
+  };
+}
+
+/*
+* asynchronous action creators
+*/
+
+export function deleteInvoice(invoiceId, callback) {
+  return function (dispatch) {
+    dispatch(isFetching());
+    const url = `${DB_URL}/invoices/${invoiceId}`;
+    fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'DELETE',
+    })
+      .then(response => response.json())
+      .then((responseJSON) => {
+        if (responseJSON.error) {
+          dispatch(handleError(responseJSON.error));
+        } else {
+          dispatch(deleteInvoiceComplete(invoiceId));
+          callback();
+        }
+      })
+      .catch(err => handleError(err));
+  };
+}
+
+
+export function fetchInvoices() {
+  // function serializeAndPrintEntries(obj) {
+  //   const r = Object.entries(obj);
+  //   r.map(entry => {
+  //     console.log('key: ', entry[0]);
+  //     console.log('type: ', typeof entry[1]);
+  //     console.log('value: ', entry[1]);
+  //     console.log('----------------------------');
+  //     if (typeof entry[1] === 'object' && entry[1] !== null) {
+  //       serializeAndPrintEntries(entry[1]);
+  //     }
+  //   });
+  // }
+  return function (dispatch) {
+    let url = `${DB_URL}/invoices`;
+    dispatch(retrieveInvoicesBegin());
+
+    fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then((responseJSON) => {
+        if (responseJSON.errorMessage) {
+          alert(responseJSON.errorMessage);
+        } else {
+          // serializeAndPrintEntries(responseJSON);
+          dispatch(updateInvoices(responseJSON.invoices));
+        }
+      })
+      .then(() => dispatch(retrieveInvoicesComplete()))
+      .catch(err => handleError(err));
+    url = `${DB_URL}/codes`;
+    const alphabetize = (array) => {
+      function compare(a, b) {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      }
+      array.sort(compare);
+      return array;
+    };
+    fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then((responseJSON) => {
+        const { codes } = responseJSON;
+        alphabetize(codes);
+        dispatch(updateCodes(codes));
+      })
+      .then(() => fetchingComplete())
+      .catch(error => handleError(error));
+  };
+}
+
+export function createNewInvoiceBegin() {
+  const alphabetize = (array) => {
+    function compare(a, b) {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    }
+    array.sort(compare);
+    return array;
+  };
+  return function (dispatch) {
+    dispatch(clearNewInvoiceData());
+    dispatch(isFetching());
+    const url = `${DB_URL}/vendors`;
+    fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then((responseJSON) => {
+        const { vendors } = responseJSON;
+        alphabetize(vendors);
+        dispatch(updateVendorList(vendors));
+      })
+      .catch(error => alert(error));
+  };
+}
+
+export function postNewInvoice(newInvoice, callback) {
+  return function (dispatch) {
+    dispatch(postNewInvoiceBegin());
+    const { number, items } = newInvoice;
+    let {
+      date,
+      vendor,
+      total,
+    } = newInvoice;
+
+    // Format date for backend
+
+    function formatDate(t) {
+      const d = new Date(t);
+      let month = '' + (d.getMonth() + 1);
+      let day = '' + d.getDate();
+      const year = d.getFullYear();
+
+      if (month.length < 2) month = '0' + month;
+      if (day.length < 2) day = '0' + day;
+
+      return [year, month, day].join('-');
+    }
+
+    date = formatDate(date);
+
+    // format vendor for backend
+
+    vendor = {
+      id: vendor.id,
+      name: vendor.name,
+      isNew: vendor.isNew || false,
+    };
+
+    // format items for backend
+    items.forEach((item) => { // eslint-disable-next-line
+      item.amount = parseFloat(item.amount.split(',').join('')).toFixed(2);
+    });
+
+    // format total for backend
+    total = parseFloat(total).toFixed(2);
+
+    const body = JSON.stringify({
+      date,
+      vendor,
+      number,
+      total,
+      items,
+    });
+
+    fetch(`${DB_URL}/invoices`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body,
+    })
+      .then(response => response.json())
+      .then((responseJSON) => {
+        if (responseJSON.invoiceId) {
+          // eslint-disable-next-line
+          newInvoice = {
+            ...newInvoice,
+            id: responseJSON.invoiceId,
+          };
+          dispatch(postNewInvoiceComplete(newInvoice));
+          callback();
+        }
+        if (responseJSON.error) {
+          dispatch(handleError(responseJSON.error));
+        }
+      })
+      .catch(error => dispatch(handleError(error.errorMessage)));
+  };
+}
---
+++ src/invoices/reducer.js
@@ -0,0 +1,228 @@
+import * as types from '../constants';
+
+const initialState = {
+  isFetching: false,
+  invoices: [],
+  codes: [],
+  vendors: [],
+  error: '',
+  newInvoice: {
+    total: 0,
+    date: new Date().toLocaleDateString(),
+    number: '',
+    vendor: {},
+    items: [
+      {
+        amount: '',
+        code: {
+          name: '',
+        },
+      },
+    ],
+  },
+};
+
+function parseMoney(str) {
+  if (typeof str === 'string') {
+    str.substring(0,1) === '$' ? str = str.slice(1) : str; // eslint-disable-line
+    return parseFloat(str.split(',').join(''));
+  }
+  return str;
+}
+
+function parseTotal(lastTotal, nextAmount) {
+  return parseFloat(parseMoney(lastTotal) + parseMoney(nextAmount)).toFixed(2);
+}
+
+function invoices(state = initialState, action) {
+  // Handle actions
+  switch (action.type) {
+    case types.ADD_ITEM_TO_INVOICE: {
+      const items = [...state.newInvoice.items];
+      items.splice(action.index, 1, action.item);
+      return {
+        ...state,
+        newInvoice: {
+          ...state.newInvoice,
+          total: parseTotal(state.newInvoice.total, action.item.amount),
+          items: [
+            ...items,
+            initialState.newInvoice.items[0],
+          ],
+        },
+      };
+    }
+    case types.ADD_NEW_LINE:
+      return {
+        ...state,
+        newInvoice: {
+          ...state.newInvoice,
+          items: [
+            ...state.newInvoice.items,
+            {
+              id: 'temporary_id',
+              code: {},
+              amount: '',
+            },
+          ],
+        },
+      };
+    case types.ADD_VENDOR_TO_INVOICE:
+      return {
+        ...state,
+        newInvoice: {
+          ...state.newInvoice,
+          vendor: action.vendor,
+        },
+      };
+    case types.CLEAR_NEW_INVOICE_DATA:
+      return {
+        ...state,
+        newInvoice: initialState.newInvoice,
+      };
+    case types.DELETE_INVOICE_COMPLETE:
+      return {
+        ...state,
+        isFetching: false,
+        invoices: state.invoices.filter(invoice => invoice.id !== action.invoiceId),
+      };
+    case types.FETCHING_COMPLETE:
+      return {
+        ...state,
+        isFetching: false,
+      };
+    case types.HANDLE_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      };
+    case types.IS_FETCHING:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case types.ON_CHANGE_DATE:
+      return {
+        ...state,
+        newInvoice: {
+          ...state.newInvoice,
+          date: action.date,
+          period: action.period,
+          week: action.week,
+        },
+      };
+    case types.ON_CHANGE_INVOICE_NUMBER:
+      return {
+        ...state,
+        newInvoice: {
+          ...state.newInvoice,
+          number: action.number,
+        },
+      };
+    case types.ON_CHANGE_ITEM_AMOUNT: {
+      const items = [...state.newInvoice.items];
+      const item = {
+        amount: action.amount,
+        code: action.item.code,
+      };
+      items.splice(action.index, 1, item);
+      return {
+        ...state,
+        newInvoice: {
+          ...state.newInvoice,
+          items,
+        },
+      };
+    }
+    case types.ON_CHANGE_ITEM_CODE: {
+      const items = [...state.newInvoice.items];
+      const item = {
+        amount: action.item.amount,
+        code: {
+          ...action.item.code,
+          name: action.codeName,
+        },
+      };
+      items.splice(action.index, 1, item);
+      return {
+        ...state,
+        newInvoice: {
+          ...state.newInvoice,
+          items,
+        },
+      };
+    }
+    case types.ON_CHANGE_VENDOR_NAME:
+      return {
+        ...state,
+        newInvoice: {
+          ...state.newInvoice,
+          vendor: {
+            ...state.newInvoice.vendor,
+            name: action.vendorName,
+          },
+        },
+      };
+    case types.POST_NEW_INVOICE_BEGIN:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case types.POST_NEW_INVOICE_COMPLETE:
+      return {
+        ...state,
+        isFetching: false,
+        invoices: [
+          action.invoice,
+          ...state.invoices,
+        ],
+      };
+    case types.REMOVE_ITEM_FROM_INVOICE: {
+      const items = [
+        ...state.newInvoice.items.filter(item => item !== action.item),
+      ];
+      return {
+        ...state,
+        newInvoice: {
+          ...state.newInvoice,
+          items,
+        },
+      };
+    }
+    case types.RETRIEVE_INVOICES_BEGIN:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case types.RETRIEVE_INVOICES_COMPLETE:
+      return {
+        ...state,
+        isFetching: false,
+      };
+    case types.TEMPORARY_ADD_VENDOR:
+      return {
+        ...state,
+        vendors: [action.newVendor, ...state.vendors],
+      };
+    case types.UPDATE_CODES:
+      return {
+        ...state,
+        codes: action.codes,
+      };
+    case types.UPDATE_INVOICES:
+      return {
+        ...state,
+        invoices: action.invoices,
+      };
+    case types.UPDATE_VENDOR_LIST:
+      return {
+        ...state,
+        vendors: action.vendors,
+      };
+    default:
+      return state;
+  }
+}
+
+export default invoices;
