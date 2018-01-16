21cc7727a61627c28e527197034164e8f18b0f63
Jameson Welling <jamowelling@gmail.com>
2018-01-16 13:48:08 -0700

    add some basic nav tabs, include react boot strap

--- src/App.js
+++ src/App.js
@@ -2,39 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Nav, NavItem, PageHeader } from 'react-bootstrap';
+
 const DB_URL='https://postinvoice-api.herokuapp.com';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Postinvoice</h1>
 
-          <div className="nav-container">
+        <PageHeader bsClass='page-header'>
 
-            <div className="nav-tab">
-              <p>Tab 1</p>
-            </div>
-
-            <div className="nav-tab">
-              <p>Tab 2</p>
-            </div>
-
-            <div className="nav-tab">
-              <p>Tab 3</p>
-            </div>
-
-            <div className="nav-tab">
-              <p>Tab 4</p>
-            </div>
-
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Postinvoice</h1>
           </div>
 
-        </header>
+          <div>
+            <Nav
+              bsStyle="tabs"
+              bsClass=" nav nav-tabs nav-justified custom-nav"
+              justified
+              activeKey={1}
+              onSelect={() => alert('hit')}
+            >
+              <NavItem eventKey={1} href="/home">
+                Invoices
+              </NavItem>
+              <NavItem eventKey={2} title="Item">
+                Inventory Sheets
+              </NavItem>
+              <NavItem eventKey={3}>
+                Reports
+              </NavItem>
+              <NavItem eventKey={4}>
+                Declining Budget
+              </NavItem>
+            </Nav>
+          </div>
 
-
+        </PageHeader>
       </div>
     );
   }
